aef1bafb94e726baa6714b7f2bea7245cb1091f5
adriangeerre <adriangomezrepolles@gmail.com>
2021-12-08 14:50:18 +0100

    cache and plot update

--- plotly_covid_map-app.py
+++ plotly_covid_map-app.py
@@ -6,66 +6,125 @@ import plotly_express as px
 
 st.set_page_config(layout="wide")
 
-st.header("Covid19 world map")
-
-# GeoJson
-geoData = gpd.read_file("worldwide.geojson")
-cols = ["name_long","subregion","geometry"]
-geoData = geoData[cols]
-geoData = geoData.rename(columns = {'name_long': 'country'})
+st.header("Covid19 animated map")
 
 # Covid data
-file = "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv/data.csv"
-covid = gpd.read_file("covid_worlwide_weekly.csv")
-covid = covid.drop(columns=['geometry'])
-covid = covid.merge(geoData, on = 'country', how='left')
+@st.cache(allow_output_mutation=True)
+def load_input():
+	print("Loading data")
+	# Load GeoJson
+	geoData = gpd.read_file("worldwide.geojson")
+	cols = ["name_long","subregion","geometry"]
+	geoData = geoData[cols]
+	geoData = geoData.rename(columns = {'name_long': 'country'})
 
-covid[["population", "weekly_count","cumulative_count"]] = covid[["population", "weekly_count","cumulative_count"]].apply(pd.to_numeric)
-covid = covid[covid["country_code"] != ""]
-covid["ratio100K"] = (covid['cumulative_count']*100000)/covid['population']
+	# Load Covid
+	file = "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath/csv/data.csv"
+	covid = pd.read_csv(file)
+	covid = covid.merge(geoData, on = 'country', how='left')
+
+	covid[["population", "weekly_count","cumulative_count"]] = covid[["population", "weekly_count","cumulative_count"]].apply(pd.to_numeric)
+	covid = covid[covid["country_code"] != ""]
+	covid["ratio100K"] = (covid['cumulative_count']*100000)/covid['population']
+
+	return covid
+
+covid = load_input()
+
+
+## Sidebar form
+st.sidebar.header("Options")
+form = st.sidebar.form(key='sidebar_form')
 
 # Select indicator
-indic = st.sidebar.radio("Indicator", ["Cases", "Deaths"])
+indic = form.radio("Indicator", ["Cases", "Deaths"])
 
 # Select scope
-scope = st.sidebar.radio("Area", ["Worldwide", "Asia", "Europe", "Africa"])
+scope = form.selectbox("Area", ["World", "Asia", "Europe", "Africa", "North America", "South America"], index=0)
 
 # Select height and width
-height = st.sidebar.slider("Height", min_value=500, max_value=1500)
-width  = st.sidebar.slider("Width", min_value=800, max_value=2300)
+height = form.slider("Height", min_value=500, max_value=1500, value=600)
+width  = form.slider("Width", min_value=500, max_value=1500, value=900)
 
 # Select animation speed
-transition_time = st.sidebar.slider("Transition time", min_value=10, max_value=100)
-frame_time = st.sidebar.slider("Frame time", min_value=5, max_value=100)
+speed = form.selectbox("Speed", ["SuperFast", "Fast","Default", "Slow", "SuperSlow"], index=2)
 
-# Plot
-def plot_covid(data, indic, scope, height, widht, transition_time, frame_time):
-	indic = indic.lower()
-	data = data[data['indicator'] == indic]
+speed_opts = {"SuperFast": {'transition_time': 50, 'frame_time': 50},
+				"Fast": {'transition_time': 150, 'frame_time': 150},
+				"Default": {'transition_time': 300, 'frame_time': 300},
+				"Slow": {'transition_time': 500, 'frame_time': 500},
+				"SuperSlow": {'transition_time': 800, 'frame_time': 800}}
 
-	if scope == "Worldwide":
-		fig = px.choropleth(data,
-			locations="country_code",
-			color="ratio100K",
-			hover_name="country",
-			color_continuous_scale="magma_r",
-			animation_frame="year_week")
-	else:
-		scope = scope.lower()
+speed = speed_opts[speed]
+
+# Submit Sidebar
+submit_button = form.form_submit_button(label='Submit')
+
+
+## Plot
+st.cache()
+def plot_covid(data, indic, scope, height, width, speed):
+	indic2 = indic.lower()
+	data = data[data['indicator'] == indic2]
+
+	if scope == "World":
 		fig = px.choropleth(data,
 			locations="country_code",
 			color="ratio100K",
 			hover_name="country",
 			color_continuous_scale="magma_r",
 			animation_frame="year_week",
-			scope=scope)
+			labels={
+			"year_week": "Year-Week",
+			"ratio100K": "{}".format(indic),
+			},
+			title="<b>{} per 100k inhabitants</b>".format(indic))
+	else:
+		scope2 = scope.lower()
+		fig = px.choropleth(data,
+			locations="country_code",
+			color="ratio100K",
+			hover_name="country",
+			color_continuous_scale="magma_r",
+			animation_frame="year_week",
+			scope=scope2,
+			labels={
+			"year_week": "Year-Week",
+			"ratio100K": "{}".format(indic),
+			},
+			title="<b>{} per 100k inhabitants in {}</b>".format(indic, scope))
 
-	fig.update_layout(autosize=False, height=height, width=width)
-	fig.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = frame_time
-	fig.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = transition_time
+	updatemenus = [{
+	'buttons': [
+		{'args':
+			[None, {'frame': {'duration': speed["frame_time"], 'redraw': True},
+			'mode': 'immediate', 'fromcurrent': True,
+			'transition': {'duration': speed["transition_time"], 'easing': 'linear'}}],
+			'label': '&#9654;',
+			'method': 'animate'},
+		{'args':
+			[[None], {'frame': {'duration': 0, 'redraw': True},
+			'mode': 'immediate', 'fromcurrent': True,
+			'transition': {'duration': 0, 'easing': 'linear'}}],
+			'label': '&#9724;',
+			'method': 'animate'}],
+	'direction': 'right',
+	'pad': {'r': 10, 't': 70},
+	'showactive': False,
+	'type': 'buttons',
+	'x': 0.1,
+	'xanchor': 'right',
+	'y': 0,
+	'yanchor': 'top'}]
 
-	st.write(fig)
+	sliders = [dict(currentvalue={"prefix": ""})]
+	fig.update_layout(autosize=False, height=height, width=width,
+		sliders=sliders, title_font_size=20,
+		updatemenus=updatemenus)
+
+	return fig
 
 # Call plot
 if st.button("Plot {}".format(indic)):
-	plot_covid(covid, indic, scope, height, width, transition_time, frame_time)
+	fig = plot_covid(covid, indic, scope, height, width, speed)
+	st.write(fig)
